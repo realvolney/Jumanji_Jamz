c8e7502b1225f54526ee1bd6e4ae710d06808017
Volney Powlis <realvolney@gmail.com>
2023-11-15 00:23:03 -0600

    attempting to fix git state control

--- app/src/main/java/capstone/activity/results/CreateChartResult.java
+++ app/src/main/java/capstone/activity/results/CreateChartResult.java
@@ -1,4 +1,42 @@
 package capstone.activity.results;
 
 public class CreateChartResult {
+
+    private final boolean success;
+    private final String name;
+    private final String id;
+
+    private CreateChartResult(boolean success, String name, String id) {
+        this.success = success;
+        name = name;
+        this.id = id;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateChartResult{" +
+                "id= " + id + '\'' +
+                "name= " + name + '\'' +
+                "success= " + success + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+
+
+    public static class Builder {
+
+    }
 }
