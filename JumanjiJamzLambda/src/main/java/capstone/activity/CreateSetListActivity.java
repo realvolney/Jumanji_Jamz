fc44156535efd7ce11b4dda96074703237af617b
Volney Powlis <realvolney@gmail.com>
2023-11-23 13:07:43 -0600

    update to see tests

--- JumanjiJamzLambda/src/main/java/capstone/activity/CreateSetListActivity.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/CreateSetListActivity.java
@@ -1,17 +1,21 @@
 package capstone.activity;
 
 import capstone.dynamodb.SetListDAO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class CreateSetListActivity {
-    private final Logger log = LogManager.getLogManager();
+    private final Logger log = LogManager.getLogger();
     private final SetListDAO dao;
 
+    /**
+     * For Instantiating CreateSetListActivity object
+     * @param dao: the setList dao for accessing the SetList table
+     */
     @Inject
     public CreateSetListActivity(SetListDAO dao) { this.dao = dao; }
 
-    
+
 }
