64dd904df25e5c1bf942b048bd263a6cc2df9325
Volney Powlis <realvolney@gmail.com>
2023-11-23 10:58:43 -0600

    began wrting activity class

--- JumanjiJamzLambda/src/main/java/capstone/activity/CreateSetListActivity.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/CreateSetListActivity.java
@@ -1,4 +1,17 @@
 package capstone.activity;
 
+import capstone.dynamodb.SetListDAO;
+
+import javax.inject.Inject;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class CreateSetListActivity {
+    private final Logger log = LogManager.getLogManager();
+    private final SetListDAO dao;
+
+    @Inject
+    public CreateSetListActivity(SetListDAO dao) { this.dao = dao; }
+
+    
 }
