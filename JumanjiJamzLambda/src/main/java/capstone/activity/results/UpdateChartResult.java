9e79ae9dc9170666351571cfed18bab76fac35f3
Volney Powlis <realvolney@gmail.com>
2023-11-23 19:57:33 -0600

    created updateChartRequest

---
+++ JumanjiJamzLambda/src/main/java/capstone/activity/results/UpdateChartResult.java
@@ -0,0 +1,14 @@
+package capstone.activity.results;
+
+;
+
+import java.util.Set;
+
+/**
+ * This class represents a result to update a chart
+ * It is used as part of the UpdateChartActivity
+ */
+public class UpdateChartResult {
+
+
+}
