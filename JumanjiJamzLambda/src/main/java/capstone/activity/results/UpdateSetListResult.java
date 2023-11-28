3c6df6b9d43175e3ebd00f8264fe7cc74f020f3f
Volney Powlis <realvolney@gmail.com>
2023-11-28 17:28:56 -0600

    created UpdateSetListResult object

--- JumanjiJamzLambda/src/main/java/capstone/activity/results/UpdateSetListResult.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/results/UpdateSetListResult.java
@@ -1,4 +1,37 @@
 package capstone.activity.results;
 
+import capstone.models.SetListModel;
+
 public class UpdateSetListResult {
+    private SetListModel setList;
+
+    private UpdateSetListResult(SetListModel setList) {
+        this.setList = setList;
+    }
+
+    public SetListModel getSetList() {
+        return setList;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateSetListResult{" +
+                "setList=" + setList +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public Builder builder() { return new Builder(); }
+    public static class Builder {
+        private SetListModel setList;
+
+        public Builder withSetList(SetListModel setList) {
+            this.setList = setList;
+            return this;
+        }
+
+        public UpdateSetListResult build() {
+            return new UpdateSetListResult(setList);
+        }
+    }
 }
