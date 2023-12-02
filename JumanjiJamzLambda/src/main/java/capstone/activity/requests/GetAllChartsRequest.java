be90c07af1587b230b2e6aa6bbce73291754ae73
Volney Powlis <realvolney@gmail.com>
2023-12-02 16:01:01 -0600

    getting up to date

--- JumanjiJamzLambda/src/main/java/capstone/activity/requests/GetAllChartsRequest.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/requests/GetAllChartsRequest.java
@@ -1,4 +1,35 @@
 package capstone.activity.requests;
 
 public class GetAllChartsRequest {
+    private final String id;
+
+    private GetAllChartsRequest(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "GetAllChartsRequest{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() { return new Builder(); }
+    public static class Builder {
+        private String id;
+
+        public Builder withId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public GetAllChartsRequest build() {
+            return new GetAllChartsRequest(id);
+        }
+    }
 }
--- JumanjiJamzLambda/src/main/java/capstone/activity/results/GetAllChartsResult.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/results/GetAllChartsResult.java
@@ -1,4 +1,50 @@
 package capstone.activity.results;
 
+import capstone.models.ChartModel;
+
+import java.util.List;
+
 public class GetAllChartsResult {
+    private final String id;
+    private final List<ChartModel> charts;
+
+    private GetAllChartsResult(String id, List<ChartModel> charts) {
+        this.id = id;
+        this.charts = charts;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<ChartModel> getCharts() {
+        return charts;
+    }
+
+    @Override
+    public String toString() {
+        return "GetAllChartsResult{" +
+                "id='" + id + '\'' +
+                ", charts=" + charts +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() { return new Builder(); }
+    public static class Builder {
+        private String id;
+        private List<ChartModel> charts;
+
+        public Builder withId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withCharts(List<ChartModel> charts) {
+            this.charts = charts;
+            return this;
+        }
+
+
+    }
 }
