76569e2854b81260297a4d6e3b1b97aeb42bacdd
Volney Powlis <realvolney@gmail.com>
2023-12-02 16:26:19 -0600

    wrote helper class for chartModel

---
+++ JumanjiJamzLambda/src/test/java/capstone/helper/ChartTestHelper.java
@@ -0,0 +1,32 @@
+package capstone.helper;
+
+import capstone.dynamodb.models.Chart;
+
+import java.util.*;
+
+public class ChartTestHelper {
+    private ChartTestHelper() {
+    }
+
+    public static List<Chart> generateChartList(int size) {
+        List<Chart> chartList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            chartList.add(generateChart(i));
+        }
+        return chartList;
+    }
+
+    public static Chart generateChart(int sequence) {
+        Chart chart = new Chart();
+        chart.setId(UUID.randomUUID());
+        chart.setName("name" + sequence);
+        chart.setArtist("artist" + sequence);
+        chart.setBpm(sequence);
+        chart.setContent("content" + sequence);
+        chart.setGenres(new HashSet<>(Arrays.asList("Funk", "Soul", String.valueOf(sequence))));
+        chart.setMadeBy("madeBy" + sequence);
+
+        return chart;
+    }
+}
