ffffd817b29a0defc4750acd96a9cf7bb93eb903
Volney Powlis <realvolney@gmail.com>
2023-12-09 14:58:43 -0600

    caught up the jumanji jamzClient methods

--- web/src/api/jumanjiJamzClient.js
+++ web/src/api/jumanjiJamzClient.js
@@ -5,7 +5,8 @@ import Authenticator from "./authenticator";
 export default class JumanjiJamzClient extends BindingClass {
     constructor(props = {}) {
         super();
-        const methodsToBind = ['clientLoaded', 'getIdentity', 'login', 'logout', 'getTokenOrThrow'];
+        const methodsToBind = ['clientLoaded', 'getIdentity', 'login', 'logout', 'getTokenOrThrow',
+            'handleError', 'getChart', 'getSetList', 'createChart', 'createSetList', ''];
         this.bindClassMethods(methodsToBind, this);
 
         this.authenticator = new Authenticator();
@@ -58,6 +59,23 @@ export default class JumanjiJamzClient extends BindingClass {
             return await this.authenticator.getUserToken();
         }
 
+    /**
+    * Helper method to log the error and run any error functions.
+    * @param error The error received from the server.
+    * @param errorCallback (Optional) A function to execute if the call fails.
+    */
+    handleError(error, errorCallback) {
+        console.error(error);
+        const errorFromApi = error?.response?.data?.error_message;
+        if (errorFromApi) {
+            console.error(errorFromApi)
+            error.message = errorFromApi;
+        }
+        if (errorCallback) {
+            errorCallback(error);
+        }
+    }
+
     // Method for getting single chart with id param
     async getChart(id, errorCallBack) {
         try {
@@ -109,8 +127,8 @@ export default class JumanjiJamzClient extends BindingClass {
 
             const payload = {
                 name: setListDetails.name,
-                charts: charts,
-                genres: genres
+                charts: setListDetails.charts,
+                genres: setListDetails.genres
             };
 
             const response = await this.axiosClient.post(`setlists`, payload, {
@@ -124,4 +142,59 @@ export default class JumanjiJamzClient extends BindingClass {
             this.handleError(error, errorCallback);
         }
     }
+
+    // Method for accessing the getAllCharts API
+    async getAllCharts(id, errorCallback) {
+        try {
+            const response = await this.axiosClient.get (`all/charts/${id}`);
+            return response.data;
+        } catch (error) {
+            this.handleError(error, errorCallback);
+        }
+    }
+
+    // Method for accessing the updateChart API
+    async updateChart(id, chartDetails, errorCallback) {
+        try {
+            const token = await this.getTokenOrThrow("Only authenticated users can update a chart");
+            const payload = {
+                name: chartDetails.name,
+                artist: chartDetails.artist,
+                bpm: chartDetails.bpm,
+                content: chartDetails.content,
+                genres: chartDetails.genres
+            };
+
+            const response = await this.put(`charts/${id}`, payload, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError(error, errorCallback);
+        }
+    }
+
+    // Method for accessing the updateSetList API
+    async updateSetList(id, setListDetails, errorCallback) {
+        try {
+            const token = await this.getTokenOrThrow("Only authenticated users can update a setList");
+            const payload = {
+                name: setListDetails.name,
+                charts: setListDetails.charts,
+                genres: setListDetails.genres
+            };
+
+            const response = await this.put(`setlists/${id}`, payload, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError(error, errorCallback);
+        }
+    }
+}
 
