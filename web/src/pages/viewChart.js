3995d6e59b02abc4beab59689bc18f5f69b037ca
Volney Powlis <realvolney@gmail.com>
2023-12-13 00:15:25 -0600

    added viewChart.js

---
+++ web/src/pages/viewChart.js
@@ -0,0 +1,36 @@
+import JumanjiJamzClient from "../api/JumanjiJamzClient";
+import Header from "../components/header";
+import BindingClass from "../util/bindingClass";
+import DataStore from "../util/DataStore";
+
+/**
+ * Logic needed to get and View Chart
+ */
+class ViewChart extends BindingClass {
+    constructor() {
+        super();
+        this.bindClassMethods(['clientLoaded', 'mount']);
+        this.header = new Header(this.dataStore);
+        console.log("viewChart constructor");
+
+    }
+
+    /**
+     * Once client is loaded, get the Chart and its metaData
+     */
+    async clientLoaded() {
+        this.client = new JumanjiJamzClient();
+        const urlParams = new URLSearchParams(window.location.search);
+        const chartId = urlParams.get('id');
+        document.getElementById('chart-name').innerText = "Loading Chart ..."
+
+        const chart = await this.client.getChart(chartId);
+        this.dataStore.set('chart', chart);
+        document.getElementById()
+        
+    }
+
+    /**
+     * Add the header to the page
+     */
+}
