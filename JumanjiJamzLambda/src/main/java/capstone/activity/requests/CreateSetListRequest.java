5eb6ca12d2f364bf5839fe66afa92dd2daef741d
Volney Powlis <realvolney@gmail.com>
2023-12-13 23:08:08 -0600

    fixed createSetList endpoint

--- JumanjiJamzLambda/src/main/java/capstone/activity/requests/CreateSetListRequest.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/requests/CreateSetListRequest.java
@@ -2,21 +2,34 @@ package capstone.activity.requests;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Set;
+
+
+import static capstone.utils.CollectionUtils.copyToSet;
+
 @JsonDeserialize(builder = CreateSetListRequest.Builder.class)
 public class CreateSetListRequest {
     private final String name;
     private final Set<String> charts;
     private final Set<String> genres;
     private final String madeBy;
-
+    private Logger log = LogManager.getLogger();
 
     private CreateSetListRequest(String name, Set<String> charts, Set<String> genres, String madeBy) {
+
         this.name = name;
+        log.info("name {}", name);
         this.charts = charts;
+        log.info("charts {}", charts);
         this.genres = genres;
+        log.info("genres {}", genres);
         this.madeBy = madeBy;
+        log.info("name {}", genres);
+
+
     }
 
     public String getName() {
@@ -62,17 +75,17 @@ public class CreateSetListRequest {
         }
 
         public Builder withCharts(Set<String> charts) {
-            this.charts = charts;
+            this.charts = copyToSet(charts);
             return this;
         }
 
         public Builder withGenres(Set<String> genres) {
-            this.genres = genres;
+            this.genres = copyToSet(genres);
             return this;
         }
 
-        public Builder withMadeBy(String name) {
-            this.name = name;
+        public Builder withMadeBy(String madeBy) {
+            this.madeBy = madeBy;
             return this;
         }
 
--- JumanjiJamzLambda/src/main/java/capstone/lambda/CreateSetListLambda.java
+++ JumanjiJamzLambda/src/main/java/capstone/lambda/CreateSetListLambda.java
@@ -4,6 +4,8 @@ import capstone.activity.requests.CreateSetListRequest;
 import capstone.activity.results.CreateSetListResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -13,18 +15,23 @@ public class CreateSetListLambda
     implements RequestHandler<AuthenticatedLambdaRequest<CreateSetListRequest>, LambdaResponse> {
 
     private final URLDecoder decoder = new URLDecoder();
+    private Logger log = LogManager.getLogger();
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<CreateSetListRequest> input, Context context) {
 
         return super.runActivity(
                 () -> {
                     CreateSetListRequest unauthenticatedRequest = input.fromBody(CreateSetListRequest.class);
+                    log.info("name {}", unauthenticatedRequest.getName());
+                    log.info("charts {}", unauthenticatedRequest.getCharts());
+                    log.info("genres {}", unauthenticatedRequest.getGenres());
                     return input.fromUserClaims(claims ->
+
                             CreateSetListRequest.builder()
                                     .withName(unauthenticatedRequest.getName())
                                     .withCharts(unauthenticatedRequest.getCharts())
                                     .withGenres(unauthenticatedRequest.getGenres())
-                                    .withMadeBy(decoder.decode(claims.get("id"), StandardCharsets.UTF_8))
+                                    .withMadeBy(decoder.decode(claims.get("email"), StandardCharsets.UTF_8))
                                     .build());
                 },
                 (request, serviceComponent) ->
