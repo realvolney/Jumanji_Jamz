03aaba56aef361683efda8d7f5ab0b83cd2665bb
Volney Powlis <realvolney@gmail.com>
2023-12-15 02:37:49 -0600

    created dao method

--- JumanjiJamzLambda/src/main/java/capstone/activity/results/SearchChartResult.java
+++ JumanjiJamzLambda/src/main/java/capstone/activity/results/SearchChartResult.java
@@ -1,4 +1,44 @@
 package capstone.activity.results;
 
+import capstone.models.ChartModel;
+
+import java.util.List;
+
+import static capstone.utils.CollectionUtils.copyToList;
+
 public class SearchChartResult {
+    private final List<ChartModel> charts;
+
+    private SearchChartResult(List<ChartModel> charts) {
+        this.charts = charts;
+    }
+
+    public List<ChartModel> getCharts() {
+        return charts;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchChartResult{" +
+                "charts=" + charts +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private List<ChartModel> charts;
+
+        public Builder withCharts(List<ChartModel> charts) {
+            this.charts = copyToList(charts);
+            return this;
+        }
+
+        public SearchChartResult build() {
+            return new SearchChartResult(charts);
+        }
+    }
 }
