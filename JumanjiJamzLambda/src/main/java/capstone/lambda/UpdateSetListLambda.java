6b7c92f542f900ca6dabfe6a532b2b22141787e4
Volney Powlis <realvolney@gmail.com>
2023-12-14 20:12:55 -0600

    updateSetList BE working on

--- JumanjiJamzLambda/src/main/java/capstone/lambda/UpdateSetListLambda.java
+++ JumanjiJamzLambda/src/main/java/capstone/lambda/UpdateSetListLambda.java
@@ -5,23 +5,32 @@ import capstone.activity.results.UpdateSetListResult;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class UpdateSetListLambda extends LambdaActivityRunner<UpdateSetListRequest, UpdateSetListResult>
     implements RequestHandler<AuthenticatedLambdaRequest<UpdateSetListRequest>, LambdaResponse> {
 
-
+    private Logger log = LogManager.getLogger();
+    private final URLDecoder decoder = new URLDecoder();
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateSetListRequest> input, Context context) {
         return super.runActivity(
                 () -> {
                     UpdateSetListRequest unauthenticatedRequest = input.fromBody(UpdateSetListRequest.class);
-                    return input.fromUserClaims(claims ->
+                    UpdateSetListRequest claimsRequest = input.fromUserClaims((claims) -> UpdateSetListRequest.builder()
+                            .withMadeBy(decoder.decode(claims.get("email"), StandardCharsets.UTF_8))
+                            .build());
+                    return input.fromPath(path ->
                             UpdateSetListRequest.builder()
-                                    .withId(unauthenticatedRequest.getId())
+                                    .withId(decoder.decode(path.get("id"), StandardCharsets.UTF_8))
                                     .withName(unauthenticatedRequest.getName())
                                     .withCharts(unauthenticatedRequest.getCharts())
                                     .withGenres(unauthenticatedRequest.getGenres())
-                                    .withMadeBy(claims.get("email"))
+                                    .withMadeBy(claimsRequest.getMadeBy())
                                     .build());
 
                 },
