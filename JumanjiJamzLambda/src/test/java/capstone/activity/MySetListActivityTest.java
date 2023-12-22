52541dc4132429826c29ea521366e71cb6cce049
Volney Powlis <realvolney@gmail.com>
2023-12-21 20:10:08 -0600

    wrote all tsts for backend

--- JumanjiJamzLambda/src/test/java/capstone/activity/MySetListActivityTest.java
+++ JumanjiJamzLambda/src/test/java/capstone/activity/MySetListActivityTest.java
@@ -1,12 +1,22 @@
 package capstone.activity;
 
+import capstone.activity.requests.MySetListsRequest;
+import capstone.activity.results.MySetListsResult;
+import capstone.converters.ModelConverter;
 import capstone.dynamodb.SetListDAO;
+import capstone.dynamodb.models.SetList;
+import capstone.helper.SetListTestHelper;
 import capstone.metrics.MetricsPublisher;
+import capstone.models.SetListModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 public class MySetListActivityTest {
@@ -28,9 +38,17 @@ public class MySetListActivityTest {
     @Test
     void handleRequest_validRequest_validResoponse() {
         // GIVEN
-
+        String madeBy = "me";
+        List<SetList> setLists = SetListTestHelper.generateSetListList(10);
+        MySetListsRequest request = MySetListsRequest.builder()
+                .withMadeBy(madeBy)
+                .build();
+        when(dao.getMySetLists(madeBy)).thenReturn(setLists);
         // WHEN
-
+        MySetListsResult result = activity.handleRequest(request);
         // THEN
+        assertEquals(result.getSetLists(),new ModelConverter()
+                .toSetListModelList(setLists));
+
     }
 }
